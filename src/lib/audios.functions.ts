f9f9736d2ce219d3e94436eb7ccb4ca3e9aa2eea
Igu <igu@mdev.com>
2026-06-02 15:06:04 -0300

    fix: import createServerFn from @tanstack/react-start

--- src/lib/audios.functions.ts
+++ src/lib/audios.functions.ts
@@ -1,4 +1,4 @@
-import { createServerFn } from "@tanstack/start";
+import { createServerFn } from "@tanstack/react-start";
 import { supabaseAdmin } from "@/integrations/supabase/client.server";
 
 async function sendVoiceToChat(opts: {
